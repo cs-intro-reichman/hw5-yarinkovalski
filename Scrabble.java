4e97170ac7b406b0547e313a46dfa0101ba852b2
Yarin Kovalski <yarin.kovalski@post.runi.ac.il>
2024-12-07 21:02:59 +0200

    final try 2

--- Scrabble.java
+++ Scrabble.java
@@ -157,7 +157,7 @@ public class Scrabble {
 			if (!isWordInDictionary(input))
 				System.out.println("No such word in the dictionary.Try again.\n");
 
-			if (!subsetOf(input,hand))
+			else if (!subsetOf(input,hand))
 				System.out.println("Invalid word. Try again.");
 
 			if (subsetOf(input,hand) && isWordInDictionary(input))
